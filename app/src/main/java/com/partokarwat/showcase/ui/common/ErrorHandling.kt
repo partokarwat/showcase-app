efaeadbfab9afd0e095903d550bc75a8d93bdcb6
Parto Karwat <partokarwat@gmail.com>
2024-10-28 10:50:03 +0100

    Add more specific http error handling

--- app/src/main/java/com/partokarwat/showcase/ui/common/ErrorHandling.kt
+++ app/src/main/java/com/partokarwat/showcase/ui/common/ErrorHandling.kt
@@ -6,7 +6,12 @@ import java.io.IOException
 
 fun getErrorStringRes(exception: Throwable?): Int =
     when (exception) {
-        is HttpException -> R.string.http_error_text
+        is HttpException ->
+            when (exception.code()) {
+                in 400..499 -> R.string.client_error_text
+                in 500..599 -> R.string.server_error_text
+                else -> R.string.http_error_text
+            }
         is IOException -> R.string.network_error_text
         else -> R.string.unknown_error_text
     }
